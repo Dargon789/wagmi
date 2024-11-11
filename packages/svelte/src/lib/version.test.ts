30f56a6ee94debcb2b400e630a04e3ca8e30ec0b
ByteAtATime <byteatatime@proton.me>
2024-11-11 14:49:01 -0800

    refactor: change svelte folders to align with other projects

--- packages/svelte/src/lib/version.test.ts
+++
@@ -1,6 +0,0 @@
-import { expect, test } from 'vitest'
-import { getVersion } from './version.js'
-
-test('default', () => {
-  expect(getVersion()).toMatchInlineSnapshot(`"@wagmi/svelte@x.y.z"`)
-})
--- packages/svelte/src/lib/version.ts
+++
@@ -1,1 +0,0 @@
-export const getVersion = () => `@wagmi/svelte@${__VERSION__}`
